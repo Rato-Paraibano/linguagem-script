7a70ce15ecbbed31cbb7722ed0c6ad869bdb7f7f
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-03-11 20:16:41 +0000

    avanço

---
+++ scripts/function.js
@@ -0,0 +1,20 @@
+function imprimirMsg(texto){
+    alert('Você clicou no botão ' + texto);
+}
+function login(){
+    const emailCorreto = "gab@gmail.com";
+    const senhaCorreta = 'senha';
+    var email = document.getElementById('email').value;
+    //alert('E-mail digitado: ' + email)
+    var senha = document.getElementById('pwd').value;
+    //alert('Senha digitada: ' + senha)
+//     if(email == emailCorreto && senha == senhaCorreta){
+//         alert('E-mail e senha corretos.' );
+//     } else print('E-mail ou senha incorretos.')
+// }
+    if(email == emailCorreto){
+        if(senha == senhaCorreta)
+        alert('E-mail e senha corretos.');
+    } else {
+        alert('E-mail incorreto.')
+    }
